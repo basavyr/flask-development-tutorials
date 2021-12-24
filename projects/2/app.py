9d42998cd0d199c59397dad9fecfc4d0a7f8a043
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-12-24 10:45:18 +0200

    update routes with data

--- projects/2/app.py
+++ projects/2/app.py
@@ -19,3 +19,22 @@ def ShowUser():
 def main_route():
     # show the index.html template
     return render_template("index.html", usr=ShowUser())
+
+
+# create a route with some data
+@app.route("/data")
+def data_route():
+    return render_template("data.html")
+
+
+def Create_User_Data(data_file):
+    # check of the data file exists
+    if os.path.isfile(data_file):
+        with open(data_file, "r") as file:
+            data = file.readlines()
+    else:
+        data = ['inexistent file']
+    return data
+
+
+print(Create_User_Data("data.txt"))
