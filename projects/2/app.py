de75f7a947d77464541006961b0244b29ac86cfb
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-12-24 11:12:07 +0200

    tries to update a list on button press

--- projects/2/app.py
+++ projects/2/app.py
@@ -40,3 +40,8 @@ def main_route():
 @app.route("/data")
 def data_route():
     return render_template("data.html", data=Create_User_Data("data.txt"))
+
+
+@app.route("/items", methods=['GET', 'POST'])
+def items():
+    items = ['item1', 'item2', 'item3']
