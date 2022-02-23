de9e8f9a866aac90ad082ffadf8e05dec56c5935
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-02-23 12:14:03 +0200

    add a navbar with custom headers

--- projects/cloud-mqtt/app.py
+++ projects/cloud-mqtt/app.py
@@ -2,17 +2,29 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 
 
-def sayhi():
-    return f'Hello there'
-
-
 app = Flask(__name__)
 
+PORT=5055
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
+@app.route("/s1")
+def service_1():
+    return render_template("service1.html")
+
+
+@app.route("/s2")
+def service_2():
+    return render_template("service2.html")
+
+
+@app.route("/s3")
+def service_3():
+    return render_template("service3.html")
+
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5055)
+    app.run(debug=True, port=PORT)
