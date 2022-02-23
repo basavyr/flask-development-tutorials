c6decaa882300c2ead4e0e52b111c98ca44522d9
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-02-23 13:13:03 +0200

    add service-1 with url input

--- projects/cloud-mqtt/app.py
+++ projects/cloud-mqtt/app.py
@@ -4,24 +4,30 @@ from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
-PORT=5055
+PORT = 5055
+
+
+def deliver_content(arg):
+    return f'{arg}-{arg}'
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
-@app.route("/s1")
-def service_1():
-    return render_template("service1.html")
+@app.route('/s1/')
+@app.route('/s1/<url_input>')
+def service_1(url_input=None):
+    return render_template("service1.html", input_name=url_input, delivered_content=deliver_content(url_input))
 
 
-@app.route("/s2")
+@app.route("/s2/")
 def service_2():
     return render_template("service2.html")
 
 
-@app.route("/s3")
+@app.route("/s3/")
 def service_3():
     return render_template("service3.html")
 
