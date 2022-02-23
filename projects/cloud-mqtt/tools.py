a61126991cfbc01f977bb6a98898806390751410
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-02-23 20:04:50 +0200

    add functions for getting system stats

--- projects/cloud-mqtt/tools.py
+++ projects/cloud-mqtt/tools.py
@@ -1,6 +1,8 @@
+from re import L
 import time
 from datetime import date, datetime
 import os
+import psutil
 
 
 def show_system_info():
@@ -17,14 +19,56 @@ def detalied_info_stats():
     return my_dict
 
 
+def show_disk_info():
+    disk_partitions = [p.device for p in psutil.disk_partitions()]
+    disk_usage = psutil.disk_usage('/').percent
+
+    return disk_partitions, disk_usage
+
+
+def show_MEM_info():
+    # MEM
+    virtual_memory = psutil.virtual_memory()
+    virt_memory = {
+        "total": virtual_memory.total,
+        "used": virtual_memory.used,
+        "percentage": virtual_memory.percent,
+    }
+
+    return virt_memory
+
+
+def show_SWAP_info():
+    swap_memory = psutil.swap_memory()
+    swap_memory = {
+        "total": swap_memory.total,
+        "used": swap_memory.used,
+        "percentage": swap_memory.percent,
+    }
+
+    return swap_memory
+
+
+def show_CPU_info():
+    # CPU
+    cpu_count = psutil.cpu_count()
+    cpugetloadavg = psutil.getloadavg()
+    cpu_times = {
+        "cpu-count": cpu_count,
+        "load-average": cpugetloadavg,
+        "cpu-percent": psutil.cpu_percent(),
+    }
+
+    return cpu_times
+
+
 def get_time():
     return datetime.utcnow()
 
 
 def main():
-    x = detalied_info_stats()
-    for key in x:
-        print(x[key])
+    cpu = show_CPU_info()
+    print(cpu)
 
 
 if __name__ == '__main__':
