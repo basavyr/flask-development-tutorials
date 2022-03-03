1582a2edf65997510be7c8ecd58ac599f2eb38bc
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-03-03 15:26:50 +0200

    add functions for generating hosts

--- projects/openstack-topology/src/create_db.py
+++ projects/openstack-topology/src/create_db.py
@@ -3,11 +3,54 @@ import sqlite3 as db
 from sqlite3 import Error
 
 from contextlib import closing
+from datetime import datetime
+import random
+
+OPENSTACK_SERVICE_TYPE = ['nova-scheduler',
+                          'nova-compute',
+                          'nova-consoleauth',
+                          'nova-conductor',
+                          'nova-controller']
 
 
 def generate_data():
     data = 1
     return data
+
+
+def get_timestamp():
+    return datetime.utcnow()
+
+
+def generate_nipne_host(index):
+    return f'dual{index}-c.cloudifin.nipne.ro'
+
+
+def generate_cloudifin_host(label):
+    return f'dual-{label}.cloudifin'
+
+
+def generate_bcsh_host(index):
+    return f'bchs{index}'
+
+
+def generate_dual_host(label):
+    return f'dual-{label}'
+
+
+def generate_controller_host(index):
+    return f'ctrl-os-{index}'
+
+
+def generate_host(idx):
+    hosts = ['ctrl-os',
+             'dual-a',
+             'dual-c',
+             'dual-d',
+             'bchs',
+             'dual1-c.cloudifin.nipne.ro',
+             'dual-d'
+             ]
 
 
 def db_connect(db_file):
@@ -19,10 +62,11 @@ def db_connect(db_file):
         return -1, -1
     else:
         print('All good with the db stuff')
+
         return db_conn, db_cursor
 
 
-def main():
+def db_init():
     dbFile = 'openstack_topology.db'
 
     db_object = db_connect(db_file=dbFile)
@@ -39,6 +83,10 @@ def main():
                                               state text, 
                                               updated text)''')
         connection.commit()
+
+
+def main():
+    print(get_timestamp())
 
 
 if __name__ == '__main__':
