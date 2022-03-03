5c19f999db95dc02c0f27c17a94f3913ee57d031
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-03-03 15:13:51 +0200

    add the topolgy example

--- projects/openstack-topology/src/create_db.py
+++ projects/openstack-topology/src/create_db.py
@@ -1,13 +1,44 @@
 import sqlite3 as db
 
+from sqlite3 import Error
+
+from contextlib import closing
+
+
+def generate_data():
+    data = 1
+    return data
+
+
+def db_connect(db_file):
+    try:
+        db_conn = db.connect(db_file)
+        db_cursor = db_conn.cursor()
+    except Error as err:
+        print(f'There was an issue with establishing database connection')
+        return -1, -1
+    else:
+        print('All good with the db stuff')
+        return db_conn, db_cursor
+
 
 def main():
-    database_name = 'openstack_topology.db'
-    database = db.connect(database_name)
+    dbFile = 'openstack_topology.db'
 
-    cursor = database.cursor()
+    db_object = db_connect(db_file=dbFile)
 
-    print(cursor)
+    connection = db_object[0]
+    cursor = db_object[1]
+    with closing(connection) as db_conn:
+        cursor.execute('''DROP TABLE IF EXISTS HOSTS''')
+        cursor.execute('''CREATE TABLE HOSTS (id_cloud integer primary_key
+                                              service text,
+                                              host text,
+                                              zone text,
+                                              status text, 
+                                              state text, 
+                                              updated text)''')
+        connection.commit()
 
 
 if __name__ == '__main__':
