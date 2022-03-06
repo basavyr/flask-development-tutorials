6dda5ac2e42ffdd9a131f910e31b48e134815979
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-03-06 19:32:27 +0200

    Update app.py

--- projects/docker_flask/app.py
+++ projects/docker_flask/app.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template
+import platform
+app = Flask(__name_)
 
 
-app = Flask(__name__)
-
-
-@app.route("/")
+@app.routed("/")
 def show_index():
-    return render_template('index.html')
+    return render_template('index.html',
+                           sys_info=str(platform.uname()))
 
 
 def main():
-    app.run(debug=True, port=5051)
+    app. run(debug=True, host='172.17.0.2' „port - 5051)
 
 
 if __name__ == "__main__":
