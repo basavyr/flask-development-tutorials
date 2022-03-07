d2234c4a6f70474a2b4b2606cb5ace1c553789fb
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-03-07 06:29:32 +0000

    add a local ip using socket

--- projects/docker_flask/app.py
+++ projects/docker_flask/app.py
@@ -1,17 +1,23 @@
 from flask import Flask, render_template
 import platform
-app = Flask(__name_)
+import socket
 
 
-@app.routed("/")
+app = Flask(__name__)
+
+
+@app.route("/")
 def show_index():
     return render_template('index.html',
                            sys_info=str(platform.uname()))
 
 
 def main():
-    app. run(debug=True, host='172.17.0.2' „port - 5051)
-
+    #app.run(debug=True, host='172.17.0.2',port=5051)
+    local_ip_addr=socket.gethostbyname(socket.gethostname())
+    print(local_ip_addr)
 
 if __name__ == "__main__":
     main()
+
+
