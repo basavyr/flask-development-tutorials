5ecaa39f33b6d23d5aaa0c5bec69b6ba0b60003d
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-03-10 10:59:47 +0200

    add basic socket io emitters

--- projects/socketio_basic/app.py
+++ projects/socketio_basic/app.py
@@ -1,7 +1,9 @@
-from distutils.log import debug
-from socket import socket
-from flask import Flask, render_template
+# Start with a basic flask app webpage.
 from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, url_for, copy_current_request_context
+from random import random
+from time import sleep
+from threading import Thread, Event
 
 
 import src.local_tools as tools
@@ -13,12 +15,24 @@ LOCALHOST = '127.0.0.1'
 # define the flask app
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
+app.config['DEBUG'] = True
+
 socketio = SocketIO(app)
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['GET'])
 def show_index():
     return render_template('index.html')
+
+
+@socketio.on('connect')
+def test_connect():
+    emit('my response', {'data': 'Connected'})
+
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
 
 
 @socketio.on('message')
@@ -28,7 +42,7 @@ def handle_message(data):
 
 def main():
     # app.run(debug=True, port=PORT)
-    socketio.run(app,port=PORT,host=LOCALHOST,debug=True)
+    socketio.run(app, port=PORT, host=LOCALHOST, debug=True)
 
 
 if __name__ == '__main__':
