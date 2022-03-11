f98d37aa78087372826745f5babd23be4ef2a7c0
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-03-11 09:20:46 +0200

    add proper event for connection

--- projects/socketio_basic/app.py
+++ projects/socketio_basic/app.py
@@ -32,35 +32,39 @@ def show_index():
 @socketio.on('connect')
 def test_connect():
     print('Connection Established ')
-    emit('connection response', {'data': 'Connected'})
+    emit('connection_response',
+         {
+             'data': 'Connected',
+             'time': tools.get_time()
+         })
 
 
-@socketio.on('disconnect')
+@ socketio.on('disconnect')
 def test_disconnect():
     print('Client disconnected')
 
 
 # define the tree test channels
-@socketio.event
+@ socketio.event
 def channel1(data):
     print('received args on channel1: ' + data)
     # emit('channel1 response', {'data': data})
 
 
-@socketio.on('channel2')
+@ socketio.on('channel2')
 def channel2(data):
     print('received args on channel2: ' + data)
     # emit('channel2 response', {'data': 'channel2 response'})
 
 
-@socketio.on('channel3')
+@ socketio.on('channel3')
 def channel2(data):
     print('received args on channel3: ' + data)
     # emit('channel3 response', {'data': 'channel3 response'})
 
 
 # log any incoming message that was emitted by the client
-@socketio.on('message')
+@ socketio.on('message')
 def handle_unnamed_message(data):
     print('...(received from client)...')
     print(f'Server-side message: {data}')
