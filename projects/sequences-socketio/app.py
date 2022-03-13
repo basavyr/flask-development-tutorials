358d74c1a7f651a5301197e34764ec4df58804ba
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-03-13 09:15:34 +0200

    improve sequence generator and bg task

--- projects/sequences-socketio/app.py
+++ projects/sequences-socketio/app.py
@@ -1,3 +1,4 @@
+# general imports
 from flask_socketio import SocketIO
 from flask_socketio import emit
 from flask import Flask, render_template
@@ -6,7 +7,7 @@ import time
 from threading import Thread, Event
 from threading import Lock
 
-
+# local imports
 import src.seq as tools
 
 # define the port and host that the app will run on
@@ -18,7 +19,6 @@ HOST = '127.0.0.1'
 # different async modes, or leave it set to None for the application to choose
 # the best option based on installed packages.
 async_mode = None
-# threads
 thread = None
 thread_lock = Lock()
 
@@ -33,22 +33,28 @@ socketio = SocketIO(app, async_mode=async_mode)
 
 
 # define the background thread
-def background_thread():
+def background_thread(arg1, arg2):
     """Example of how to send server generated events to clients."""
-    count = 0
-    limit = 20
+
+    # testing the bg task with extra arguments
+    print('value of the arg1 is ' + str(arg1))
+    print('value of the arg2 is ' + str(arg2))
+
+    count = 1
+    limit = arg2
 
     ok = True
     while ok:
-        # generate a new random sequence every 5 seconds
+        # generate a new random sequence every arg1 seconds
         # print('generating a new random sequence...')
-        socketio.sleep(5)
+        socketio.sleep(arg1)
         socketio.emit('sequence',
                       {"sequence": tools.get_sequence(), }
                       )
-        count += 1
         if count == limit:
+            print('task limit exceeded. stopping the background task...')
             ok = False
+        count += 1
 
 
 @app.route("/", methods=['GET'])
@@ -59,11 +65,16 @@ def show_index():
 
 @socketio.on('connect')
 def on_connect():
+    # define the arguments to be used in the background task
+    # arg1 represents the time between each iteration
+    # arg2 represents the number of iterations
+
     global thread
     with thread_lock:
         if thread is None:
             print('server -> starting the background thread...')
-            thread = socketio.start_background_task(background_thread)
+            thread = socketio.start_background_task(
+                background_thread, arg1=5, arg2=3)
 
 
 @socketio.event
