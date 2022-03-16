8c9c8e0a07b146296a341a4340967fa5bb215b7e
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-03-16 10:36:14 +0200

    retrieve the values for the dropdown

--- projects/mqtt-socketio/static/app.js
+++ projects/mqtt-socketio/static/app.js
@@ -1,3 +1,11 @@
 $(document).ready(() => {
-  console.log("Connection is good");
+  console.log("Connection is OK");
+
+  // console log the text from the dropdown list
+  $("#topic").change(() => {
+    // console.log($("#topic").val());
+    var element_text = $("#topic option:selected").text();
+    var element_value = $("#topic option:selected").val();
+    console.log("{ " + element_value + " }" + " -> " + element_text);
+  });
 });
