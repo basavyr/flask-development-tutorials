e4b544a0d8a865de52bbbea0d2c1597c0cbeb760
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-03-16 10:40:19 +0200

    create submission button for each client value

--- projects/mqtt-socketio/static/app.js
+++ projects/mqtt-socketio/static/app.js
@@ -8,4 +8,9 @@ $(document).ready(() => {
     var element_value = $("#topic option:selected").val();
     console.log("{ " + element_value + " }" + " -> " + element_text);
   });
+
+  $("#client-submit").click(() => {
+    var sub_value = $("#topic option:selected").val();
+    console.log("submitted the client: " + sub_value);
+  });
 });
