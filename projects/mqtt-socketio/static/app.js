86a53e4a1fb958d909c9ee312705fda1f166bf5e
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-03-16 16:31:53 +0200

    get topic data from select

--- projects/mqtt-socketio/static/app.js
+++ projects/mqtt-socketio/static/app.js
@@ -6,10 +6,10 @@ $(document).ready(() => {
   });
 
   // console log the text from the dropdown list
-  $("#topic").change(() => {
+  $("#client").change(() => {
     var element_text = $("#topic option:selected").text();
     var element_value = $("#topic option:selected").val();
-    console.log("TOPIC: { " + element_value + " }" + " -> " + element_text);
+    console.log("CLIENT: { " + element_value + " }" + " -> " + element_text);
   });
 
   //get the information from the message dropdown list
@@ -19,12 +19,23 @@ $(document).ready(() => {
     console.log("MSG: { " + element_value + " }" + " -> " + element_text);
   });
 
+  //get the information from the topic dropdown list
+  $("#topic").change(() => {
+    var element_text = $("#topic option:selected").text();
+    var element_value = $("#topic option:selected").val();
+    console.log("TOPIC: { " + element_value + " }" + " -> " + element_text);
+  });
+
   $("#client-submit").click(() => {
     var client = $("#topic option:selected").val();
     var msg = $("#message option:selected").val();
     // console.log("submitted the client: " + sub_value);
     $("#client-show").html(
-      "<p> Client <strong>" + client + "</strong> will publish message: <strong>" + msg + "</strong>.</p>"
+      "<p> Client <strong>" +
+        client +
+        "</strong> will publish message: <strong>" +
+        msg +
+        "</strong>.</p>"
     );
     socketio.emit("client-request", { topic: topic, msg: msg });
   });
