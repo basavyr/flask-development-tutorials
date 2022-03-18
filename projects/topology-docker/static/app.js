efed74d8f5ac5cba64e9e93e48aeb5ed082b6894
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-03-18 10:00:37 +0200

    add request to get the db

--- projects/topology-docker/static/app.js
+++ projects/topology-docker/static/app.js
@@ -1,1 +1,8 @@
-console.log("App started successfully");
+$("document").ready(() => {
+  console.log("App started successfully");
+  sio = io();
+
+  $("#docker_view").click(() => {
+    sio.emit("get_container_db");
+  });
+});
