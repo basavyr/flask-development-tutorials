6dba6023037cde3a3abbb57471d0b34b7e999b13
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-03-18 09:45:30 +0200

    add skeleton for app

---
+++ projects/topology-docker/app.py
@@ -0,0 +1,45 @@
+# general imports
+from flask_socketio import SocketIO
+from flask_socketio import emit
+from flask import Flask, render_template
+from random import random
+import time
+from threading import Thread, Event
+from threading import Lock
+
+
+# define the port and host that the app will run on
+PORT = 5051
+HOST = '127.0.0.1'
+
+
+# Set this variable to "threading", "eventlet" or "gevent" to test the
+# different async modes, or leave it set to None for the application to choose
+# the best option based on installed packages.
+# source of the doc => https://github.com/miguelgrinberg/Flask-SocketIO/blob/main/example/app.py
+async_mode = None
+
+thread = None
+thread_lock = Lock()
+
+
+# define the flask app
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+app.config['DEBUG'] = True
+
+# define the socketio object
+socketio = SocketIO(app, async_mode=async_mode)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def show_index():
+    return render_template('index.html')
+
+
+def main():
+    socketio.run(app, port=PORT, host=HOST)
+
+
+if __name__ == '__main__':
+    main()
