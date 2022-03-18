62d035aa4281770980ec11a2645cb53462a739b5
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-03-18 20:17:42 +0200

    add skeleton for the db adder

--- projects/topology-docker/src/container_db.py
+++ projects/topology-docker/src/container_db.py
@@ -1,3 +1,4 @@
+from curses import raw
 import subprocess
 from subprocess import PIPE
 import sqlite3 as db
@@ -5,6 +6,11 @@ from contextlib import closing
 
 
 UTF8 = 'utf-8'
+
+
+def manipulate_raw_string(raw_string):
+    raw_string = [line.split() for line in str(raw_string).strip().split('\n')]
+    return raw_string[1:]
 
 
 def get_active_containers():
@@ -18,7 +24,7 @@ def get_active_containers():
         # return stderr.decode(UTF8)
         return []
     else:
-        return stdout.decode(UTF8)
+        return manipulate_raw_string(stdout.decode(UTF8))
 
 
 def get_all_containers():
@@ -31,18 +37,15 @@ def get_all_containers():
     except AssertionError as issue:
         return []
     else:
-        return stdout.decode(UTF8)
-
-
-def manipulate_raw_string(raw_string):
-    return 1
+        return manipulate_raw_string(stdout.decode(UTF8))
 
 
 def create_container_db():
-    DB_NAME = 'containers.docker.db'
-    db_conn = db.connect(DB_NAME)
-    with closing(db_conn):
-        cursor = db_conn.cursor()
+    DB_FILE = 'containers.docker.db'
+
+    DB_CONN = db.connect(DB_FILE)
+    with closing(DB_CONN):
+        cursor = DB_CONN.cursor()
         cursor.execute('DROP TABLE IF EXISTS CONTAINERS')
         cursor.execute('''CREATE TABLE IF NOT EXISTS CONTAINERS
                                 (id integer primary_key,
@@ -50,17 +53,22 @@ def create_container_db():
                                 container_name, text
                                 container_status text)''')
 
-        db_conn.commit()
+        DB_CONN.commit()
+
+    return DB_CONN
+
+
+def add_containers_to_db(containers):
+    conn = create_container_db()
+    for container in containers:
+        print(f'will add this container to the db: {container}')
 
 
 def main():
     containers_active = get_active_containers()
     containers_all = get_all_containers()
 
-    # create_container_db()
-
-    print(containers_active)
-    print(containers_all)
+    add_containers_to_db(containers_active)
 
 
 if __name__ == '__main__':
