6fd32ce45051f223b33dfd1fe3f1e524db0370bb
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-03-19 19:54:34 +0200

    add buttons for each docker view

--- projects/topology-docker/static/app.js
+++ projects/topology-docker/static/app.js
@@ -1,15 +1,21 @@
 $("document").ready(() => {
   console.log("App started successfully");
   sio = io();
-  
+
   $("#docker_view").click(() => {
     sio.emit("get_container_db");
   });
-  
+
   // set the added-text div to hidden
   $("#added-text").hide();
 
-  $("#map-button").click(() => {
+  $("#tabular-view").click(() => {
+    console.log("User clicked the topology button");
+    //show the added-text div
+    $("#added-text").toggle();
+    // $("#added-text").html("<p>Will show the map.</p>");
+  });
+  $("#topology-view").click(() => {
     console.log("User clicked the topology button");
     //show the added-text div
     $("#added-text").toggle();
