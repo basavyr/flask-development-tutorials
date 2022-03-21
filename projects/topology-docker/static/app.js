932feeefe2e8ac8aaf4df6521abffc5f99a6e030
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-03-21 16:49:54 +0200

    add more styling to the docker views

--- projects/topology-docker/static/app.js
+++ projects/topology-docker/static/app.js
@@ -58,7 +58,8 @@ $("document").ready(() => {
       } else {
         container_div = '<div class="container-inactive">';
       }
-      container_div += "<p>" + container_topology[i] + "</p>";
+      container_div +=
+        "<p> <strong>Container #" + container_topology[i][0] + "</strong></p>";
       container_div += "</div>";
       tabular_html += container_div;
     }
