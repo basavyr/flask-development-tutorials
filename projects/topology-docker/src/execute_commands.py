eb59285ac4fcaacc3df2cc016f2eda751fd0691e
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-03-21 21:55:28 +0200

    Update execute_commands.py

--- projects/topology-docker/src/execute_commands.py
+++ projects/topology-docker/src/execute_commands.py
@@ -9,11 +9,13 @@ def execute_docker_command(command, docker_id):
     process = subprocess.Popen(cmd, stdout=PIPE, stderr=PIPE)
     stdout, stderr = process.communicate()
 
-    print(stdout)
-    print(stderr)
+    if (stdout.decode('utf-8').strip() == str(docker_id)):
+        print('Docker container was successfully changed')
+
     try:
         assert stderr == b'', 'Issue while executing the command'
     except AssertionError as issue:
+        print(issue)
         return -1
     else:
         return stdout.decode('utf-8')
