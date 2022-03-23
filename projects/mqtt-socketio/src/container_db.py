de6296c22bcc913ac7b17f4d5b55375b5781df37
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-03-23 19:30:13 +0200

    Update container_db.py

--- projects/mqtt-socketio/src/container_db.py
+++ projects/mqtt-socketio/src/container_db.py
@@ -1,12 +1,14 @@
+import gc
 from re import S
 import subprocess
 from subprocess import PIPE
 import sqlite3 as db
+from webbrowser import get
 
 UTF8 = 'utf-8'
 
 
-def get_containers():
+def get_running_containers():
     """
     - retrieve a list of containers
     """
@@ -23,23 +25,34 @@ def get_containers():
         return stdout.decode(UTF8)
 
 
-def manipulate_container_string(raw_string):
-    raw_dockers = raw_string.strip()
+def get_container_ids(raw_list):
+    raw_dockers = raw_list.strip()
     raw_dockers = str(raw_dockers).split('\n')[1:]
+
     dockers = [x.split() for x in raw_dockers]
-    # print('Containers:')
-    # for container in dockers:
-    #     print(container)
+
     docker_ids = [container[0] for container in dockers]
+
+    return docker_ids
+
+
+def get_container_names(raw_list):
+    raw_dockers = raw_list.strip()
+    raw_dockers = str(raw_dockers).split('\n')[1:]
+
+    dockers = [x.split() for x in raw_dockers]
+
     docker_names = [container[1] for container in dockers]
-    # print(docker_ids)
-    # print(docker_names)
-    return docker_ids, docker_names
+
+    return docker_names
 
 
 def main():
-    raw_containers = get_containers()
-    manipulate_container_string(raw_containers)
+    raw_containers = get_running_containers()
+    docker_containers_id = get_container_ids(raw_containers)
+    docker_containers_names = get_container_names(raw_containers)
+    print(docker_containers_id)
+    print(docker_containers_names)
 
 
 if __name__ == '__main__':
