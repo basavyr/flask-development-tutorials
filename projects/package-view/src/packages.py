55d813918646c3ec299536dc63880f4497e0b283
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-05-22 05:32:17 +0300

    add proper pack extractor

--- projects/package-view/src/packages.py
+++ projects/package-view/src/packages.py
@@ -1,10 +1,9 @@
-from pickle import EMPTY_DICT
-from re import sub
-import sqlite3
 import subprocess
 from subprocess import PIPE
+from subprocess import STDOUT
 
-EMPTY_LIST = []
+# return ann empty list of errors occurred during the execution of the command
+EMPTY_LIST = ['No packages found']
 
 
 def get_yum_packages():
@@ -22,3 +21,28 @@ def get_yum_packages():
         with open('yum.packages.dat', 'w+') as writer:
             writer.write(str(packages))
         return packages
+
+
+def get_brew_packages():
+    """Retrieves all the manually installed packages on the system"""
+
+    cmd = ['brew', 'list', '--versions']
+
+    proc = subprocess.Popen(
+        cmd, stdout=PIPE, stderr=PIPE)
+
+    try:
+        stdout, stderr = proc.communicate(timeout=5)
+    except subprocess.TimeoutExpired:
+        proc.kill()
+        return EMPTY_LIST
+    else:
+        # extract the packages as raw strings that are split only for new lines
+        raw_packages = str(stdout.decode('utf-8')).strip().split('\n')
+
+        # extract each package by splitting after whitespaces
+        packages = [pack.split() for pack in raw_packages]
+        print(packages[0])
+
+
+get_brew_packages()
